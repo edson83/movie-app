081c6df5262a30c1a13dda09b386a7c1f4d57f91
Edson Shiino <edsonshiino@gmail.com>
2019-02-27 08:11:39 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
+
+import { MovieService } from './movie.service';
+import { GenreComponent } from './genre/genre.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'movie/:movieID', component: MovieComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MovieComponent,
+    GenreComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [ MovieService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
